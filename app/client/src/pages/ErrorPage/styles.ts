5105f16189e49294f1138ad64f6d3a326de43db2
srikant-code <srikant.sahoo@mayadata.io>
2020-10-26 19:41:00 +0530

    Added Team page and other changes in footer
    Signed-off-by: srikant-code <srikant.sahoo@mayadata.io>

--- app/client/src/pages/ErrorPage/styles.ts
+++ app/client/src/pages/ErrorPage/styles.ts
@@ -3,31 +3,40 @@ import { makeStyles } from "@material-ui/core/styles";
 // Component styles
 export const useStyles = makeStyles((theme) => ({
 	root: {
-		height: "100%",
+		height: "100vh",
+		width: "100vw",
 		padding: theme.spacing(2),
 		display: "flex",
-		flexDirection: "row",
+		flexFlow: "row wrap",
 		justifyContent: "center",
+		alignContent: "center",
 		background: "linear-gradient(180deg,rgba(255, 136, 0, 0.65),rgba(229, 46, 113, 0.65))",
-		[theme.breakpoints.down("sm")]: {
-			marginTop: "10%",
-		},
 		[theme.breakpoints.down("xs")]: {
-			flexDirection: "column",
+			flexFlow: "row wrap",
 			alignItems: "center",
-		},
-		[theme.breakpoints.up("lg")]: {
-			marginTop: "5%",
+			justifyContent: "center",
 		},
 	},
 	mainText: {
 		fontSize: "50px",
 		color: "#000000",
+		lineHeight: "10.5rem",
 		[theme.breakpoints.up("xl")]: {
 			fontSize: "60px",
 		},
 		[theme.breakpoints.down("sm")]: {
+			lineHeight: "6rem",
 			fontSize: "30px",
 		},
 	},
+	text404: {
+		fontSize: "370px",
+		color: "#000",
+		[theme.breakpoints.up("xl")]: {
+			fontSize: "370px",
+		},
+		[theme.breakpoints.down("sm")]: {
+			fontSize: "190px",
+		},
+	}
 }));
