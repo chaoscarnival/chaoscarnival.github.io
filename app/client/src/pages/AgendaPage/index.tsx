a6619cb1f782da91bce95560313d3c8aa577ff03
Amit Kumar Das <amitkumar.das@mayadata.io>
2021-01-29 23:33:40 +0530

    Added speakers modal
    Signed-off-by: Amit Kumar Das <amitkumar.das@mayadata.io>

--- app/client/src/pages/AgendaPage/index.tsx
+++ app/client/src/pages/AgendaPage/index.tsx
@@ -1,15 +1,50 @@
-import { Typography } from "@material-ui/core";
+import { Modal, Typography } from "@material-ui/core";
 import * as React from "react";
+import { useState } from "react";
 import "../../scrollbar.css";
 import data from "./data";
 import { useStyles } from "./styles";
 
+interface PopOverModal {
+	modalState: boolean;
+	speakerImg: string;
+	speakerName: string;
+	speakerRole: string;
+	sessionName: string;
+	sessionTime: string;
+	sessionDescription: string;
+	sessionTags: string[];
+}
+
 const AgendaPage: React.FC = () => {
 	const classes = useStyles();
-
-	const Speaker = (image: string, name: string, role: string) => {
+	const [modalStatus, setModalStatus] = useState<PopOverModal>({
+		modalState: false,
+		speakerImg: "",
+		speakerName: "",
+		speakerRole: "",
+		sessionName: "",
+		sessionTime: "",
+		sessionDescription: "",
+		sessionTags: [""],
+	});
+	const Speaker = (image: string, name: string, role: string, d: any) => {
 		return (
-			<div className={classes.speaker}>
+			<div
+				className={classes.speaker}
+				onClick={() => {
+					setModalStatus({
+						modalState: true,
+						speakerName: name,
+						speakerRole: role,
+						speakerImg: image,
+						sessionName: d.sessionName,
+						sessionTime: "",
+						sessionDescription: d.description,
+						sessionTags: d.tags,
+					});
+				}}
+			>
 				<img src={image} id="image" alt="speakerImg" />
 				<div style={{ marginTop: "1rem" }}>
 					<Typography id="subtitle">{name}</Typography>
@@ -18,7 +53,19 @@ const AgendaPage: React.FC = () => {
 			</div>
 		);
 	};
-
+	console.log(modalStatus);
+	const handleClose = () => {
+		setModalStatus({
+			modalState: false,
+			speakerImg: "",
+			speakerName: "",
+			speakerRole: "",
+			sessionName: "",
+			sessionTime: "",
+			sessionDescription: "",
+			sessionTags: [""],
+		});
+	};
 	const Keynote = (title?: string, speaker?: Array<Object>) => {
 		return (
 			<>
@@ -29,7 +76,8 @@ const AgendaPage: React.FC = () => {
 							{Speaker(
 								Object.values(s)[2],
 								Object.values(s)[0],
-								Object.values(s)[1]
+								Object.values(s)[1],
+								speaker
 							)}
 						</>
 					))}
@@ -104,7 +152,8 @@ const AgendaPage: React.FC = () => {
 														{Speaker(
 															Object.values(s)[2],
 															Object.values(s)[0],
-															Object.values(s)[1]
+															Object.values(s)[1],
+															d.speaker1
 														)}
 													</>
 												);
@@ -125,7 +174,8 @@ const AgendaPage: React.FC = () => {
 														{Speaker(
 															Object.values(s)[2],
 															Object.values(s)[0],
-															Object.values(s)[1]
+															Object.values(s)[1],
+															d.speaker2
 														)}
 													</>
 												);
@@ -136,7 +186,36 @@ const AgendaPage: React.FC = () => {
 							</div>
 						);
 					})}
+					s
 				</span>
+				<Modal
+					open={modalStatus.modalState}
+					onClose={handleClose}
+					style={{
+						margin: "auto",
+						padding: 50,
+						height: "70%",
+						width: "60%",
+						backgroundColor: "#161A1D",
+					}}
+				>
+					<>
+						<Typography className={classes.title}>
+							{modalStatus.speakerName}
+						</Typography>
+						<Typography>{modalStatus.sessionName}</Typography>
+						{modalStatus.sessionTags.map((a) => {
+							return (
+								<Typography style={{ color: "white" }}>
+									{a}
+								</Typography>
+							);
+						})}
+						<Typography style={{ color: "white" }}>
+							{modalStatus.sessionTime}
+						</Typography>
+					</>
+				</Modal>
 			</div>
 			<br />
 			{/* Footer */}
