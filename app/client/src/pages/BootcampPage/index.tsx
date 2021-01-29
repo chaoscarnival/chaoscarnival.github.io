ea52483398fef227b9b69da18b17ac4d0d0bede7
ishangupta-ds <ishan.gupta@mayadata.io>
2021-01-29 15:23:34 +0530

    adding sponsor booths, bootcamps and agenda page layout
    Signed-off-by: ishangupta-ds <ishan.gupta@mayadata.io>

---
+++ app/client/src/pages/BootcampPage/index.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import Bootcamps from "../../components/Bootcamps/index";
+import Footer from "../../components/Footer";
+import MainHeader from "../../components/Header";
+import JoinCarnival from "../../components/JoinCarnival";
+import Sponsors from "../../components/Sponsors";
+import "../../scrollbar.css";
+import { useStyles } from "./styles";
+
+function BootcampPage() {
+	const classes = useStyles();
+
+	React.useEffect(() => {
+		window.scrollTo(0, 0);
+	});
+
+	return (
+		<div className="scrollbar scrollbar-primary">
+			<div className={classes.rootContainer}>
+				<a id="home" href="home">
+					{" "}
+				</a>
+				<MainHeader inHomePage={true} linksActive={true} />
+				<div className={classes.root}>
+					<div style={{ marginTop: "auto" }}>
+						<a id="bootcamps" href="bootcamps">
+							{" "}
+						</a>
+						<Bootcamps />
+					</div>
+
+					<div style={{ marginTop: "auto" }}>
+						<a id="sponsors" href="sponsors">
+							{" "}
+						</a>
+						<Sponsors />
+					</div>
+				</div>
+
+				<div style={{ marginTop: "auto" }}>
+					<JoinCarnival />
+				</div>
+
+				<div style={{ marginTop: "auto" }}>
+					<Footer />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default BootcampPage;
---
+++ app/client/src/pages/SponsorBoothsPage/styles.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+	rootContainer: {
+		height: "100vh",
+		display: "flex",
+		flexDirection: "column",
+	},
+	root: {
+		textAlign: "center",
+	},
+}));
