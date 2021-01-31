eb271320cfc79e7394752df06a911e86445e5632
Vansh Bhatia <vansh.bhatia@mayadata.io>
2021-01-31 14:40:34 +0530

    Improved design consistency, added data directory, base setup for sponsorBooths page (#127)
    Signed-off-by: Vansh Bhatia <vansh.bhatia@mayadata.io>

--- app/client/src/pages/SponsorBoothsPage/index.tsx
+++ app/client/src/pages/SponsorBoothsPage/index.tsx
@@ -1,53 +1,28 @@
 import * as React from "react";
-import Bootcamps from "../../components/Bootcamps/index";
+import Bootcamps from "../../components/Bootcamps";
 import Footer from "../../components/Footer";
 import MainHeader from "../../components/Header";
 import JoinCarnival from "../../components/JoinCarnival";
 import Sponsors from "../../components/Sponsors";
 import "../../scrollbar.css";
-import bootcamps from "../../utils/bootcampData";
+import bootcamps from "../../data/bootcampData";
 import { useStyles } from "./styles";
 
 function SponsorBoothsPage() {
 	const classes = useStyles();
 
-	React.useEffect(() => {
-		window.scrollTo(0, 0);
-	});
-
 	return (
 		<div className="scrollbar scrollbar-primary">
-			<div className={classes.rootContainer}>
-				<a id="home" href="home">
-					{" "}
-				</a>
-				<MainHeader />
-				<div className={classes.root}>
-					<div style={{ marginTop: "auto" }}>
-						<a id="bootcamps" href="bootcamps">
-							{" "}
-						</a>
-						<Bootcamps bootcamps={bootcamps} />
-					</div>
-
-					<div style={{ marginTop: "auto" }}>
-						<a id="sponsors" href="sponsors">
-							{" "}
-						</a>
-						<Sponsors />
-					</div>
-				</div>
-
-				<div style={{ marginTop: "auto" }}>
-					<JoinCarnival />
-				</div>
-
-				<div style={{ marginTop: "auto" }}>
-					<Footer />
-				</div>
+			<MainHeader />
+			<div className={classes.root}>
+				<Bootcamps bootcamps={bootcamps} />
+				<Sponsors />
+				<JoinCarnival />
+				<Footer />
 			</div>
 		</div>
 	);
 }
 
 export default SponsorBoothsPage;
+
--- app/client/src/pages/SponsorBoothsPage/styles.ts
+++ app/client/src/pages/SponsorBoothsPage/styles.ts
@@ -1,12 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyles = makeStyles((theme) => ({
-	rootContainer: {
-		height: "100vh",
-		display: "flex",
-		flexDirection: "column",
-	},
 	root: {
-		textAlign: "center",
 	},
 }));
