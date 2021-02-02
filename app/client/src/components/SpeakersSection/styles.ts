e828f7f8bf467eee533ff4dbd7351b5f909d5719
ishangupta-ds <ishan.gupta@mayadata.io>
2021-02-02 13:22:50 +0530

    Fixes for speaker images
    Signed-off-by: ishangupta-ds <ishan.gupta@mayadata.io>

--- app/client/src/components/SpeakersSection/styles.ts
+++ app/client/src/components/SpeakersSection/styles.ts
@@ -254,47 +254,4 @@ export const useStyles = makeStyles((theme: Theme) => ({
 			marginLeft: "-5vw",
 		},
 	},
-	agendaSection: {
-		paddingTop: "5rem",
-		margin: "0 auto",
-		[theme.breakpoints.down("xs")]: {
-			marginLeft: "8vw",
-		},
-	},
-	detailText: {
-		fontSize: "1.8rem",
-		fontWeight: 500,
-		textAlign: "left",
-		[theme.breakpoints.down("md")]: {
-			fontSize: "1.5rem",
-		},
-		[theme.breakpoints.down("sm")]: {
-			fontSize: "1.4rem",
-		},
-		[theme.breakpoints.down("xs")]: {
-			fontSize: "1.4rem",
-		},
-	},
-	agendaText: {
-		fontSize: "1.8rem",
-		fontWeight: 700,
-		textAlign: "left",
-		width: "fit-content",
-		WebkitBackgroundClip: "text",
-		WebkitTextFillColor: "transparent",
-		color: "transparent",
-		backgroundClip: "text",
-		background: "linear-gradient(89.85deg, #BCA1F7 0.13%, #EE759C 98.01%)",
-		paddingLeft: theme.spacing(1),
-		paddingRight: theme.spacing(1),
-		[theme.breakpoints.down("md")]: {
-			fontSize: "1.5rem",
-		},
-		[theme.breakpoints.down("sm")]: {
-			fontSize: "1.4rem",
-		},
-		[theme.breakpoints.down("xs")]: {
-			fontSize: "1.4rem",
-		},
-	},
 }));
