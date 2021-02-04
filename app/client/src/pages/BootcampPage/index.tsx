0f61ad9ce75eec5b6faf94434a2164a318cfbadd
ishangupta-ds <ishan.gupta@mayadata.io>
2021-02-04 17:39:12 +0530

    Website SEO updates.
    Signed-off-by: ishangupta-ds <ishan.gupta@mayadata.io>

--- app/client/src/pages/BootcampPage/index.tsx
+++ app/client/src/pages/BootcampPage/index.tsx
@@ -1,5 +1,6 @@
 import { Typography } from "@material-ui/core";
 import * as React from "react";
+import { Helmet } from "react-helmet";
 import Bootcamps from "../../components/Bootcamps";
 import Footer from "../../components/Footer";
 import MainHeader from "../../components/Header";
@@ -13,18 +14,32 @@ function BootcampPage() {
 	const classes = useStyles();
 
 	return (
-		<div className="scrollbar scrollbar-primary">
-			<div className={classes.rootContainer}>
-				<MainHeader />
-				<div className={classes.root}>
-					<Typography className={classes.title}>
-						Chaos Engineering Bootcamps
-					</Typography>
-					<Bootcamps bootcamps={bootcamps} />
+		<div>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>Chaos Carnival | 2021 Chaos Engineering Bootcamps</title>
+				<meta
+					name="keywords"
+					content="chaos carnival 2021, chaos carnival bootcamps, chaos engineering bootcamps, chaos engineering 101, learn chaos engineering, getting started with chaos engineering, running chaos experiments, chaos engineering for Kafka, chaos engineering for percona, creating chaos workflows"
+				/>
+				<link
+					rel="canonical"
+					href="https://chaoscarvival.io/bootcamps"
+				/>
+			</Helmet>
+			<div className="scrollbar scrollbar-primary">
+				<div className={classes.rootContainer}>
+					<MainHeader />
+					<div className={classes.root}>
+						<Typography className={classes.title}>
+							Chaos Engineering Bootcamps
+						</Typography>
+						<Bootcamps bootcamps={bootcamps} />
+					</div>
+					<Sponsors />
+					<JoinCarnival />
+					<Footer />
 				</div>
-				<Sponsors />
-				<JoinCarnival />
-				<Footer />
 			</div>
 		</div>
 	);
--- app/client/src/pages/SponsorshipPage/index.tsx
+++ app/client/src/pages/SponsorshipPage/index.tsx
@@ -1,5 +1,6 @@
 import { Divider, Typography } from "@material-ui/core";
 import * as React from "react";
+import { Helmet } from "react-helmet";
 import Footer from "../../components/Footer";
 import MainHeader from "../../components/Header";
 import SponsorBenefits from "../../components/SponsorBenefits";
@@ -12,53 +13,67 @@ function SponsorshipPage() {
 	const classes = useStyles();
 
 	return (
-		<div className="scrollbar scrollbar-primary">
-			<div className={classes.rootContainer}>
-				<MainHeader />
-				<div>
-					{/* Header component */}
-					<div className={classes.headerDiv}>
-						<div className={classes.headerText}>
-							<Typography className={classes.mainHeader}>
-								Chaos Carnival <br></br> Sponsorship Details &
-								Benefits
-							</Typography>
-							<Typography className={classes.headerDesc}>
-								Check below about the different types of
-								sponsorships and their benefits
-							</Typography>
+		<div>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>Chaos Carnival 2021 | Sponsorship</title>
+				<meta
+					name="keywords"
+					content="Chaos carnival 2021, chaos engineering, chaos conference, chaos conference 2021, chaosconf, sponsor booths, sponsorship benefits, sponsorship, chaos engineering 101, cloud native, MayaData, CNCF, Percona, Wipro, Epsagon, Elastic, Gremlin, Soda Foundation"
+				/>
+				<link
+					rel="canonical"
+					href="https://chaoscarvival.io/sponsorship"
+				/>
+			</Helmet>
+			<div className="scrollbar scrollbar-primary">
+				<div className={classes.rootContainer}>
+					<MainHeader />
+					<div>
+						{/* Header component */}
+						<div className={classes.headerDiv}>
+							<div className={classes.headerText}>
+								<Typography className={classes.mainHeader}>
+									Chaos Carnival <br></br> Sponsorship Details
+									& Benefits
+								</Typography>
+								<Typography className={classes.headerDesc}>
+									Check below about the different types of
+									sponsorships and their benefits
+								</Typography>
+							</div>
+							<div className={classes.imgDiv}>
+								<img
+									src="icons/chaos-carnival-logo-min.png"
+									alt="Chaos Carnival Logo"
+									className={classes.headerImg}
+								/>
+							</div>
 						</div>
-						<div className={classes.imgDiv}>
-							<img
-								src="icons/chaos-carnival-logo-min.png"
-								alt="Chaos Carnival Logo"
-								className={classes.headerImg}
-							/>
+						<div className={classes.customizedTable}>
+							<CustomizedTables />
+						</div>
+						<SponsorBenefits />
+						<div
+							style={{
+								marginTop: "auto",
+								marginLeft: "auto",
+								marginRight: "auto",
+							}}
+							className={classes.sponsorSectionContainer}
+						>
+							<a id="sponsors" href="sponsors">
+								{" "}
+							</a>
+							<div className={classes.sponsorSection}>
+								<Sponsors />
+							</div>
+							<Divider className={classes.divider} />
 						</div>
 					</div>
-					<div className={classes.customizedTable}>
-						<CustomizedTables />
+					<div style={{ marginTop: "auto" }}>
+						<Footer />
 					</div>
-					<SponsorBenefits />
-					<div
-						style={{
-							marginTop: "auto",
-							marginLeft: "auto",
-							marginRight: "auto",
-						}}
-						className={classes.sponsorSectionContainer}
-					>
-						<a id="sponsors" href="sponsors">
-							{" "}
-						</a>
-						<div className={classes.sponsorSection}>
-							<Sponsors />
-						</div>
-						<Divider className={classes.divider} />
-					</div>
-				</div>
-				<div style={{ marginTop: "auto" }}>
-					<Footer />
 				</div>
 			</div>
 		</div>
