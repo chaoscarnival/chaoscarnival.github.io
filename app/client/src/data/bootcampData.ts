cc1b14fafe9e79125172ac79f94d28e0e75f2c08
Amit Kumar Das <40661238+amityt@users.noreply.github.com>
2021-03-24 14:52:33 +0530

    Updated the bootcamp section with youtube link (#209)
    * Updated the bootcamp links and added in speakers section
    * Reduced font size
    
    Signed-off-by: Amit Kumar Das <amitkumar.das@mayadata.io>

--- app/client/src/data/bootcampData.ts
+++ app/client/src/data/bootcampData.ts
@@ -5,8 +5,7 @@ const bootcamps: Bootcamp[] = [
 		title: "Bootcamp #1 - Chaos for Kafka on AWS",
 		description:
 			"This bootcamp is intended for introducing the chaos ideas for Kafka on AWS platform. This session will cover a guided tutorial where a few chaos workflows are created and executed live. Audience can participate in the chaos workflow execution along with the instructor.\n\nThose participants who are interested in doing live chaos workflows in the bootcamp can join this Slack channel to receive detailed instructions. Join ChaosCarnival slack community and then join #bootcamps slack channel",
-		joinUrl:
-			"https://www.airmeet.com/e/cabe9140-62c8-11eb-8a3f-5f90a373e3d1",
+		joinUrl: "https://youtu.be/i-paikImUCA",
 		startDateTime: "10 Feb 2021 02:00:00 +0530",
 		endDateTime: "10 Feb 2021 02:45:00 +0530",
 		slackLink: "https://chaoscarnival.slack.com/archives/C01M92XDEDN",
@@ -15,8 +14,7 @@ const bootcamps: Bootcamp[] = [
 		title: "Bootcamp #2 - Chaos for Percona on AKS",
 		description:
 			"This bootcamp is intended for introducing the chaos ideas for Percona on AKS platform. This session will cover a guided tutorial where a few chaos workflows are created and executed live. Audience can participate in the chaos workflow execution along with the instructor.\n\nThose participants who are interested in doing live chaos workflows in the bootcamp can join this Slack channel to receive detailed instructions. Join ChaosCarnival slack community and then join #bootcamps slack channel",
-		joinUrl:
-			"https://www.airmeet.com/e/cabe9140-62c8-11eb-8a3f-5f90a373e3d1",
+		joinUrl: "https://youtu.be/Jlsjrr2F8Ok",
 		startDateTime: "11 Feb 2021 02:00:00 +0530",
 		endDateTime: "11 Feb 2021 02:45:00 +0530",
 		slackLink: "https://chaoscarnival.slack.com/archives/C01M06NUS6M",
